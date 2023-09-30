089f21557ab1599f5047d919f280eaf2792c035d
FABBIO\fadhi <fadhilhameed2004@gmail.com>
2023-09-30 07:11:54 +0530

    no message

---
+++ ADMIN_PROJECT/src/Components/NavigationTemplates/NaviconTemplate/Nav.jsx
@@ -0,0 +1,8 @@
+import './Nav.css'
+
+export const Nav = () => {
+  return  <div>Nav</div>;
+  
+};
+
+export default Nav;
--- ADMIN_PROJECT/src/Components/NavigationTemplates/Navigation.jsx
+++ ADMIN_PROJECT/src/Components/NavigationTemplates/Navigation.jsx
@@ -1,14 +1,16 @@
+import Nav from './NaviconTemplate/nav';
 import './Navigation.css'
 
 const Navigation = () => {
   return  <div className = "navigation">
     <header>
         <div className="profile">
-            <img src="" alt="user-img" className="profile-img" />
+            <img src="./src/assets/pic.jpg" alt="user-img" className="profile-img" />
         </div>
         <span>Creative_Ambition</span>
     </header>
-  </div>;
+    <Nav />
+    </div>;
   
 }
 
