396f1f6108f06c08360d349f6d2b5269cb63fb56
FABBIO\fadhi <fadhilhameed2004@gmail.com>
2023-09-30 06:49:54 +0530

    REACT DASHBOARD

---
+++ ADMIN_PROJECT/src/Components/NavigationTemplates/Navigation.jsx
@@ -0,0 +1,15 @@
+import './Navigation.css'
+
+const Navigation = () => {
+  return  <div className = "navigation">
+    <header>
+        <div className="profile">
+            <img src="" alt="user-img" className="profile-img" />
+        </div>
+        <span>Creative_Ambition</span>
+    </header>
+  </div>;
+  
+}
+
+export default Navigation
